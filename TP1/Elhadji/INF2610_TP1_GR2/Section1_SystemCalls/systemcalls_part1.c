99486ba56f856aa5fea219e0831d255ad4eba4ca
Elhadji Sarr <elhadjindiayesarr20@gmail.com>
2024-09-04 21:13:42 -0400

    version 1 premiere partie

--- TP1/Elhadji/INF2610_TP1_GR2/Section1_SystemCalls/systemcalls_part1.c
+++ TP1/Elhadji/INF2610_TP1_GR2/Section1_SystemCalls/systemcalls_part1.c
@@ -5,9 +5,43 @@
  *
  * Ajoutez vos noms, prénoms et matricules
 */
+#include <stdlib.h>
+#include <unistd.h>             
+#include <stdio.h>             
+#include <fcntl.h>
+#include <string.h>
+
+const int erreur_ouverture = -1;
+const int stdout_fd = 1;
+const int stdin_fd = 0;
+const int longueur_entree = 1;
+const int read_eof = 0;
+
 
 
 int main () {
-    // TODO
+    // TODO : ouvrir le fichier nommé systemcalls_output2.txt en mode écriture et l’option O_TRUNC;
+    int fd_systemcalls = open("./systemcalls_output2.txt", O_WRONLY | O_TRUNC);
+    if (fd_systemcalls == erreur_ouverture){
+       exit(EXIT_FAILURE);
+    }
+
+    // TODO : afficher à l’écran le texte “Saisissez votre texte suivi de CTRL-D :\n". La combinaison des touches CTRL et D (ˆD) indique une fin de fichier
+    char message[] = "Saisissez votre texte suivi de CTRL-D :\n";
+    int val_write = write (stdout_fd, message, strlen(message));
+
+    // TODO : lire caractère par caractère des données à partir du clavier jusqu’à la rencontre d’une fin de fichier(ˆD)
+    // sauvegarder dans le fichier systemcalls_output2.txt chaque caractère lu à partir du clavier
+    int val_read = 1;
+    while (val_read != read_eof){
+        char entree;
+        val_read = read(stdin_fd, &entree, longueur_entree);
+        
+        int val_write_fl = write(fd_systemcalls, &entree, longueur_entree);
+    }
+
+    // fermer le fichier systemcalls_output2.txt.
+    int val_close = close(fd_systemcalls);
+
     return 0;
 }
--- TP1/Elhadji/INF2610_TP1_GR2/Section1_SystemCalls/systemcalls_part2.c
+++ TP1/Elhadji/INF2610_TP1_GR2/Section1_SystemCalls/systemcalls_part2.c
@@ -6,19 +6,46 @@
  * Ajoutez vos noms, prénoms et matricules
 */
 
-#include <stdio.h>
 #include <stdlib.h>
+#include <unistd.h>             
+#include <stdio.h>             
+#include <fcntl.h>
+#include <string.h>
+
+
+const int stdout_fd = 1;
+
+
+void conclure_part21(){
+    printf("\n");
+}
 
 
 void part21 ()
 {
-    // TODO
+    //TODO : La deuxième ligne "77dbcb01f571f1c32g196c3a7d27f62e (printed using printf)" doit être affichée
+    //en utilisant la fonction printf pour chaque caractère du message
+    char message_printf[] = "77dbcb01f571f1c32g196c3a7d27f62e (printed using printf)";
+    for (int i = 0; i < strlen(message_printf); ++i){
+        printf("%c", message_printf[i]);
+    }
+    
+    // TODO : La première ligne du message "77dbcb01f571f1c32g196c3a7d27f62e (printed using write)\n" doit
+    //être affichée en utilisant un seul appel système write.
+    char message_write[] = "77dbcb01f571f1c32g196c3a7d27f62e (printed using write)\n";
+    int val_write = write(stdout_fd, message_write, strlen(message_write));
 
+    conclure_part21();
 }
 
 void part22 ()
 {
-    // TODO
+    // TODO : omplétez la fonction part22 du fichier systemcalls_part2.c pour qu’elle débute par un
+    //appel à la fonction setvbuf, de la librairie stdio.h, avant de réaliser un traitement identique à celui de la
+    //fonction part21.
+    setvbuf(stdout ,NULL, _IONBF, 0);
+
+    part21();
     
 }
 
