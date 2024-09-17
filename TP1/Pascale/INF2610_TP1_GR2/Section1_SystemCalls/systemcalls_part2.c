69db80f813eabce6aaeb8f083c1a2b4b0369fb1e
Pascale Lafond (2204155) <pascalelafond@hotmail.com>
2024-09-17 10:40:12 -0400

    Update systemcalls_part2.c

--- TP1/Pascale/INF2610_TP1_GR2/Section1_SystemCalls/systemcalls_part2.c
+++ TP1/Pascale/INF2610_TP1_GR2/Section1_SystemCalls/systemcalls_part2.c
@@ -4,21 +4,140 @@
  * SystemCalls - part2.c
  *
  * Ajoutez vos noms, prénoms et matricules
+ * Pascale Lafond 2204155, 
 */
 
 #include <stdio.h>
 #include <stdlib.h>
-
+#include <unistd.h>
 
 void part21 ()
 {
     // TODO
+    printf("7");
+    printf("7");
+    printf("d");
+    printf("b");
+    printf("c");
+    printf("b");
+    printf("0");
+    printf("1");
+    printf("f");
+    printf("5");
+    printf("7");
+    printf("1");
+    printf("f");
+    printf("1");
+    printf("c");
+    printf("3");
+    printf("2");
+    printf("g");
+    printf("1");
+    printf("9");
+    printf("6");
+    printf("c");
+    printf("3");
+    printf("a");
+    printf("7");
+    printf("d");
+    printf("2");
+    printf("7");
+    printf("f");
+    printf("6");
+    printf("2");
+    printf("e");
+    printf(" ");
+    printf("(");
+    printf("p");
+    printf("r");
+    printf("i");
+    printf("n");
+    printf("t");
+    printf("e");
+    printf("d");
+    printf(" ");
+    printf("u");
+    printf("s");
+    printf("i");
+    printf("n");
+    printf("g");
+    printf(" ");
+    printf("p");
+    printf("r");
+    printf("i");
+    printf("n");
+    printf("t");
+    printf("f");
+    printf(")");
+    printf("\n");
+
+    write(STDOUT_FILENO, "77dbcb01f571f1c32g196c3a7d27f62e (printed using write)\n",56);
 
 }
 
 void part22 ()
 {
     // TODO
+    setvbuf(stdout, NULL, _IONBF, 0);
+    
+    printf("7");
+    printf("7");
+    printf("d");
+    printf("b");
+    printf("c");
+    printf("b");
+    printf("0");
+    printf("1");
+    printf("f");
+    printf("5");
+    printf("7");
+    printf("1");
+    printf("f");
+    printf("1");
+    printf("c");
+    printf("3");
+    printf("2");
+    printf("g");
+    printf("1");
+    printf("9");
+    printf("6");
+    printf("c");
+    printf("3");
+    printf("a");
+    printf("7");
+    printf("d");
+    printf("2");
+    printf("7");
+    printf("f");
+    printf("6");
+    printf("2");
+    printf("e");
+    printf(" ");
+    printf("(");
+    printf("p");
+    printf("r");
+    printf("i");
+    printf("n");
+    printf("t");
+    printf("e");
+    printf("d");
+    printf(" ");
+    printf("u");
+    printf("s");
+    printf("i");
+    printf("n");
+    printf("g");
+    printf(" ");
+    printf("p");
+    printf("r");
+    printf("i");
+    printf("n");
+    printf("t");
+    printf("f");
+    printf(")");
+    printf("\n");
+
+    write(STDOUT_FILENO, "77dbcb01f571f1c32g196c3a7d27f62e (printed using write)\n",56);
     
 }
 
