e5a18601fbb442945c8cdf6fcfca92d4b7ae90d7
Elhadji Sarr <elhadjindiayesarr20@gmail.com>
2024-09-18 14:45:27 -0400

    thread

--- TP1/Elhadji/INF2610_TP1_GR2/Section2_Processes/processes_part2.c
+++ TP1/Elhadji/INF2610_TP1_GR2/Section2_Processes/processes_part2.c
@@ -23,13 +23,49 @@ void* contribution(void*p)
 {
     // TODO
 
-  return NULL;
+  int no = *(int*) p;
+  long int limInferieure = (no * m/nb) + 1;
+  long int limSuperieure = (no + 1) * m / nb;
+
+  long int sommeFinal;
+  
+  sommeFinal = 0; 
+
+  for(int i = limInferieure; i <= limSuperieure; ++i){
+    sommeFinal += i;
+  }
+  somme[no] = sommeFinal;
+
+  printf("%ld \n", somme[no]);
+
+  pthread_exit(0);
 }
 
 
 void question2( )
 {
-    // TODO
-    
+  pthread_t tabTid[nb];
+  int arg1 = 0;
+  int arg2 = 1;
+  int arg3 = 2;
+  int arg4 = 3;
+  pthread_create(&tabTid[0], NULL, contribution, (void *)&arg1);
+  pthread_create(&tabTid[1], NULL, contribution, (void *)&arg2);
+  pthread_create(&tabTid[2], NULL, contribution, (void *)&arg3);
+  pthread_create(&tabTid[3], NULL, contribution, (void *)&arg4);
+  
+  for (int i = 0; i < nb; ++i){
+    pthread_join(tabTid[i], NULL);
+  }
+
+  long long sommeThread = 0;
+  for (int i = 0; i < nb; ++i){
+      sommeThread += (long long) somme[i];
+  }
+
+  long long somme= (long long)m * ((long long) (m+1))/2;
+
+  printf("%lld \n%lld \n", somme, sommeThread);
+
 }
 
